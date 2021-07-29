bf574308a8eb158475cb106e4c4a823a2a389e33
Khusan Tojiboev <khusan.tojiboev@gmail.com>
2021-07-29 12:16:31 +0500

    Added custom permission to specific instance

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 
@@ -8,7 +9,8 @@ class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    
+
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
